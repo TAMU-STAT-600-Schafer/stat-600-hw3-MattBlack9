5bf1aef14cdda364f8941f38f9228e0f662aea81
Matthew Blackley <matt9black@gmail.com>
2024-09-24 23:46:38 -0500

    adding an error for incorrect X or Xt

--- FunctionsLR.R
+++ FunctionsLR.R
@@ -20,6 +20,12 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
   # Check that the first column of X and Xt are 1s, if not - display appropriate message and stop execution.
+  if(sum(X[, 1]) != nrow(X)){
+    stop("First column of X is not 1s")
+  }
+  else{
+    stop("Working")
+  }
   
   # Check for compatibility of dimensions between X and Y
   
--- Tests.R
+++ Tests.R
@@ -1,2 +1,6 @@
 # This is a script to save your own tests for the function
 source("FunctionsLR.R")
+
+temp <- matrix(2, nrow= 4)
+
+LRMultiClass(temp, 1,1,1,1)
